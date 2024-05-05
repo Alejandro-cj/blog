8bd05f159ba95df9100e10fc08ab504f9b40b23b
Alejandro-cj <aloflow_345@hotmail.com>
2024-05-04 19:22:11 -0500

    routes pruebas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "welcome to the homepages";
 });
+
+Route::get("/posts", function () {
+    return "aqui se mostraran todos el posts ";
+});
+
+Route::get("/posts/create", function () {
+    return "aqui se mostrara un formulario para crear un post"; 
+});
+
+Route::get("/posts/{post}", function ($post) {
+    return "aqui se mostraran el post {$post}";
+});
+
+
+/*Route::get("/posts/{post}/{category}", function ($post, $category) {
+    return "aqui se mostraran los post {$post} de la categoria {$category}";
+}); */
+
+//get
+//post
+//patch
+//delete
+//
